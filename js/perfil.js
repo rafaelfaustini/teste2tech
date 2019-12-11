2676da0cc1add89f5b322b720df1fa34c1a6618f
rafaelfaustini <contato@rafaelfaustini.com.br>
2019-12-11 11:52:11 -0300

    Fake login e armazenamento de dados
    Foi feito uma simulação de login com uso do localStorage para assim a página não precisar toda vez carregar as informações de todos os usuários tendo em vista a necessidade apenas do usuário logado

--- js/perfil.js
+++ js/perfil.js
@@ -11,14 +11,26 @@ var app = new Vue({
 
     },
     mounted () {
+        var apiUsuarios = "https://jsonplaceholder.typicode.com/users"
+
+        if(localStorage.userid){ // Caso usuario já esteja "logado", pegar os dados apenas dele
+            this.userid = localStorage.userid
+            this.username = localStorage.username
+            apiUsuarios+= "/"+this.userid;
+        }
+
         axios
-          .get('https://jsonplaceholder.typicode.com/users')
+          .get(apiUsuarios)
 
           .then(response => {
-              this.usuario = response.data[4]
-              localStorage.setItem('userid', this.usuario.id)
-              localStorage.setItem('username', this.usuario.username)
-          }) // 4 é um usuário escolhido de id 5
+              if(this.userid){
+                this.usuario = response.data
+              } else{
+                this.usuario = response.data[4] // Simula antes de um 'login', onde de todos os usuários o de id 5 é 'logado'
+              }
+              localStorage.setItem('userid', this.usuario.id) // Ao "logar" o id do usuário fica armazenado, fazendo com que não seja necessário carregar todos os usuários cada vez que acessar a página
+              localStorage.setItem('username', this.usuario.username) // Também é armazenado o username
+          })
           .catch(error => {
               console.log("Houve um erro")
               console.log(error)
