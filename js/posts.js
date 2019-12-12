6155755ab7de76376ea6c28a68cc9537bd0fffb1
rafaelfaustini <contato@rafaelfaustini.com.br>
2019-12-12 10:00:16 -0300

    2 página
    2 página semi finalizada

---
+++ js/posts.js
@@ -0,0 +1,79 @@
+const usuariopadrao = 7
+
+var app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            autenticado: false,
+            erro: false,
+            usuario: null,
+            albuns: null,
+            posts: [],
+            users: []
+        }
+
+    },
+
+    methods: {
+        checarDados: function () {
+            dadoArmazenado = localStorage.getItem("userid") && localStorage.getItem("username")
+            if (!dadoArmazenado) {
+                this.erro = true; // Caso não esteja logado, dispara um erro
+            }
+
+        },
+
+        getUsers: async function (){
+            for(var i=0; i< this.posts.length;i++){
+            id = this.posts[i].userId;
+            url = "https://jsonplaceholder.typicode.com/users?id="+id
+            await axios
+                .get(url)
+
+                .then(response => {
+                        if(response.data.length){
+                            this.users.push(response.data[0].username)
+                        }
+                    })
+                .catch(error => {
+                    console.log("Houve um erro")
+                    console.log(error)
+                    this.erro = true
+                })
+                .finally(() => { return true })
+                
+            }
+        },
+
+        getPosts: async function (url) {
+            await axios
+                .get(url)
+
+                .then(response => {
+                        for (var a in response.data){
+                            var p = response.data[a]
+
+                            if(p.userId != localStorage.getItem("userid")){
+                                this.posts.push(p)
+                            }
+                        }
+                    })
+                .catch(error => {
+                    console.log("Houve um erro")
+                    console.log(error)
+                    this.erro = true
+                })
+                .finally(() => { return true })
+        }
+    },
+
+    async mounted() {
+        await this.checarDados()
+        if(!this.erro){
+            var apiPost = "https://jsonplaceholder.typicode.com/posts"
+            await this.getPosts(apiPost)
+            await this.getUsers()
+        }
+
+    }
+})
